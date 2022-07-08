358bc2c382dac6871a0360f38ce82cbb650a8225
Maxim Antsiferov <90709563+MaximAntsiferov@users.noreply.github.com>
2022-07-09 01:11:15 +0700

    try ctx

--- bot.py
+++ bot.py
@@ -54,6 +54,7 @@ async def main():
     dp = Dispatcher(bot, storage=storage)
     scheduler = ContextSchedulerDecorator(AsyncIOScheduler())
     scheduler.add_jobstore('redis', jobs_key='birthdays_jobs', run_times_key='birthdays_run_times')
+    scheduler.ctx.add_instance(bot, declared_class=Bot)
 
 
     register_all_filters(dp)
